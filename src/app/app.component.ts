bd843c1e8e33b98bf3ec33c02e5095480fd1aab3
bharat-bhushan <bharatsoni1996@gmail.com>
2019-09-19 21:52:23 +0530

    fix searching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,102 +6,32 @@ import { DataTableDirective } from 'angular-datatables';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements  OnInit{
-  @ViewChild(DataTableDirective,)
+  @ViewChild(DataTableDirective)
   datatableElement: DataTableDirective;
-  title = 'final-app';
   dtOptions: DataTables.Settings = {};
   
-  selectedKey = "First"
-  selectedSearch = ""
-  tablekeys = [
-    {
-      title: 'ID',
-      data: 'ID'
-    },
-    {
-      title: 'First',
-      data: 'First'
-    },
-    {
-      title: 'Middle',
-      data: 'Middle'
-    },
-    {
-      title: 'Last',
-      data: 'Last'
-    },
-    {
-      title: 'Sex',
-      data: 'Sex'
-    },
-    {
-      title: 'Age',
-      data: 'Age'
-    },
-    {
-      title: 'Hair Colour',
-      data: 'Hair Colour'
-    }
-  ]
-
-  data  = [
-    {
-      "ID": 1,
-      "First": "Tom",
-      "Middle": "aa",
-      "Last": "Smith",
-      "Sex": "Male",
-      "Age": 20,
-      "Hair Colour": "Red"
-    },
-    {
-      "ID": 2,
-      "First": "Andrew",
-      "Middle": "bb",
-      "Last": "Jones",
-      "Sex": "Male",
-      "Age": 25,
-      "Hair Colour": "Red"
-    },
-    {
-      "ID": 3,
-      "First": "John",
-      "Middle": "cc",
-      "Last": "Smith",
-      "Sex": "Male",
-      "Age": 30,
-      "Hair Colour": "Black"
-    },
-    {
-      "ID": 4,
-      "First": "Sarah",
-      "Middle": "dd",
-      "Last": "Jones",
-      "Sex": "Woman",
-      "Age": 35,
-      "Hair Colour": "Black"
-    },
-    {
-      "ID": 5,
-      "First": "Eve",
-      "Middle": "ee",
-      "Last": "Smith",
-      "Sex": "Woman",
-      "Age": 40,
-      "Hair Colour": "Brown"
-    },
-    {
-      "ID": 6,
-      "First": "Alice",
-      "Middle": "ff",
-      "Last": "Jones",
-      "Sex": "Woman",
-      "Age": 45,
-      "Hair Colour": "Brown"
-    }
-  ]
+  selectedKey
+  selectedSearch
+  tablekeys = []
+  defaultColSelection
+  data = []
   
   ngOnInit(): void {
+   
+    this.getDefaults()
+  }
+
+  onKeydownEvent(e) {
+    console.log("key do")
+    if (!e.target.value.length) {
+      this.getDefaults()
+      
+    }
+  }
+
+  getDefaults() {
+    this.selectedSearch = ""
+    this.selectedKey = "First"
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 2,
@@ -109,21 +39,116 @@ export class AppComponent implements  OnInit{
       searching: false,
     };
    
-   
+
+    this.tablekeys = [
+      {
+        title: 'ID',
+        data: 'ID',
+        show : true
+      },
+      {
+        title: 'First',
+        data: 'First',
+        show : true
+      },
+      {
+        title: 'Middle',
+        data: 'Middle',
+        show : true
+      },
+      {
+        title: 'Last',
+        data: 'Last',
+        show : true
+      },
+      {
+        title: 'Sex',
+        data: 'Sex',
+        show : true
+      },
+      {
+        title: 'Age',
+        data: 'Age',
+        show : true
+      },
+      {
+        title: 'Hair Colour',
+        data: 'Hair Colour',
+        show : true
+      }
+    ]
+  
+    this.data  = [
+      {
+        "ID": 1,
+        "First": "Tom",
+        "Middle": "aa",
+        "Last": "Smith",
+        "Sex": "Male",
+        "Age": 20,
+        "Hair Colour": "Red"
+      },
+      {
+        "ID": 2,
+        "First": "Andrew",
+        "Middle": "bb",
+        "Last": "Jones",
+        "Sex": "Male",
+        "Age": 25,
+        "Hair Colour": "Red"
+      },
+      {
+        "ID": 3,
+        "First": "John",
+        "Middle": "cc",
+        "Last": "Smith",
+        "Sex": "Male",
+        "Age": 30,
+        "Hair Colour": "Black"
+      },
+      {
+        "ID": 4,
+        "First": "Sarah",
+        "Middle": "dd",
+        "Last": "Jones",
+        "Sex": "Woman",
+        "Age": 35,
+        "Hair Colour": "Black"
+      },
+      {
+        "ID": 5,
+        "First": "Eve",
+        "Middle": "ee",
+        "Last": "Smith",
+        "Sex": "Woman",
+        "Age": 40,
+        "Hair Colour": "Brown"
+      },
+      {
+        "ID": 6,
+        "First": "Alice",
+        "Middle": "ff",
+        "Last": "Jones",
+        "Sex": "Woman",
+        "Age": 45,
+        "Hair Colour": "Brown"
+      }
+    ]
   }
 
   onSearch(e) {
     const { selectedSearch, selectedKey } = this;
-    console.log("")
+    console.log("selectedSearch", selectedSearch)
     const self = this
     this.data = this.data.filter((data) => {
-      return new RegExp(data[selectedKey].toLowerCase()).test(selectedSearch.toLowerCase())
+      console.log("data[selectedKey]", data[selectedKey])
+      return new RegExp(`${data[selectedKey]}`.toLowerCase()).test(selectedSearch.toLowerCase())
     })
-    console.log("this.data", this.data)
-    this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
-      console.log("dtInstance", dtInstance)
-      
-    });
+    
+  }
+
+  reset() {
+    this.getDefaults()
   }
 
   
